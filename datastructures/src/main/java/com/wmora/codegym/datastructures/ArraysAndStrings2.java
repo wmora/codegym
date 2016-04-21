1d7e8ce8b82e82b5bfde0620554b20793932b91a
William Mora <wilmor24@gmail.com>
2016-04-20 21:52:04 -0300

    javadoc [ci skip]

--- datastructures/src/main/java/com/wmora/codegym/datastructures/ArraysAndStrings2.java
+++ datastructures/src/main/java/com/wmora/codegym/datastructures/ArraysAndStrings2.java
@@ -1,5 +1,9 @@
 package com.wmora.codegym.datastructures;
 
+/**
+ * Write code to reverse a C-Style String (C-String means that “abcd” is represented as  ve characters, including
+ * the null character )
+ */
 class ArraysAndStrings2 {
 
     static String reverseCString(String cString) {
